018a54f0ee0be94e7c5439fde6e3ff5b0a84a129
asmaaMous556 <asmaamosmoh@gmail.com>
2021-08-01 22:45:32 +0200

    convert-duration-formate

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -6,6 +6,8 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { video } from 'src/app/models/video';
 import { SaveVideoService } from 'src/app/services/save-video.service';
 import { Observable, observable, Subscription } from 'rxjs';
+import {parse, end, toSeconds, pattern} from 'iso8601-duration';
+ 
 
 @Component({
   selector: 'app-details',
@@ -16,7 +18,8 @@ export class DetailsComponent implements OnInit, OnDestroy {
   id: string = '';
   VideoUrl: string = 'https://www.youtube.com/embed/';
   safeUrl: SafeResourceUrl = '';
-  video!: video;
+  video: any;
+  duration:any;
   paramsSubscripe: Subscription = new Subscription;
   videoDetailsSub: Subscription = new Subscription;
 
@@ -25,11 +28,12 @@ export class DetailsComponent implements OnInit, OnDestroy {
     private youtubeService: YoutubeService, private _sanitizer: DomSanitizer, private savevideo: SaveVideoService) { }
   ngOnDestroy(): void {
     this.paramsSubscripe.unsubscribe();
-    this.videoDetailsSub.unsubscribe
+    this.videoDetailsSub.unsubscribe();
 
   }
 
   ngOnInit(): void {
+    
     this.paramsSubscripe = this.route.params.subscribe(params => {
       this.id = params.id;
       this.VideoUrl = this.VideoUrl + this.id;
@@ -38,7 +42,7 @@ export class DetailsComponent implements OnInit, OnDestroy {
 
       this.videoDetailsSub = this.youtubeService.getSingleVideo(this.id).subscribe((data: any) => {
         this.video = data.items
-        console.log(this.video)
+        this.duration= (toSeconds(parse(this.video[0].contentDetails.duration)));
       },error=>{
        console.log(error)
       })
@@ -48,5 +52,6 @@ export class DetailsComponent implements OnInit, OnDestroy {
   saveVideo(id: string) {
     this.savevideo.saveVideoToFvList(id);
   }
+ 
 
 }
